7b1170b906e89ce1c19130a1da0c76a8e7b32bdd
SiD P <sidpanhale@gmail.com>
2023-10-12 15:47:21 +0530

    Initialized backend and frontend

--- backend/server.js
+++ backend/server.js
@@ -20,4 +20,4 @@ app.get("/api/notes/:id", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log("Server started on PORT ${PORT}"));
+app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
